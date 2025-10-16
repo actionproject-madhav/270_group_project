28cf1dbb9691c3e96a56f59fe964c3ce5a4042de
Richard Stoiberer <rstoiberer@rollins.edu>
2025-10-16 12:03:04 -0400

    Test again

--- CMS270Project/src/Hello.java
+++ CMS270Project/src/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.print("Hello this is working");
 		// TODO Auto-generated method stub
+		System.out.print("Test");
 
 	}
 
