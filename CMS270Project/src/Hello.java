751760d02c831a5464cc1bc62709255c13292405
Richard Stoiberer <rstoiberer@rollins.edu>
2025-10-16 12:07:11 -0400

    last change for today

--- CMS270Project/src/Hello.java
+++ CMS270Project/src/Hello.java
@@ -2,6 +2,7 @@
 public class Hello {
 
 	public static void main(String[] args) {
+		System.out.println("Hello from Richard");
 		System.out.print("Hello this is working");
 		// TODO Auto-generated method stub
 		System.out.print("Test");
